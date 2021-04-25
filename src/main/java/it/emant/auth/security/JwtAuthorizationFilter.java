90c6e9ef7ba94f2fd3cad4969d7c6bf0d3ea8aee
emant <joshua999x@gmail.com>
2021-04-25 18:01:18 +0200

    custom filter error

--- src/main/java/it/emant/auth/security/JwtAuthorizationFilter.java
+++ src/main/java/it/emant/auth/security/JwtAuthorizationFilter.java
@@ -1,6 +1,7 @@
 package it.emant.auth.security;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -8,17 +9,24 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import io.jsonwebtoken.Claims;
+import it.emant.auth.dto.ErrorDTO;
 import it.emant.auth.service.TokenService;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
 
     private final TokenService tokenService;
+
+    private final ObjectMapper objectMapper= new ObjectMapper();
+
 
     public JwtAuthorizationFilter(AuthenticationManager authManager, TokenService tokenService) {
         super(authManager);
@@ -27,19 +35,14 @@ public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest req,
-                                    HttpServletResponse res,
-      FilterChain chain) throws IOException, ServletException {
-      
-      String token = Optional.ofNullable(req.getHeader("AUthorization"))
-        .map(t -> t.replace("Bearer ", ""))
-        .orElse(null);
-      
-      if (token == null) {
-        chain.doFilter(req, res);
-        return;
-      }
-      
+        HttpServletResponse res,
+        FilterChain chain) throws IOException, ServletException {
+    
       try {
+        String token = Optional.ofNullable(req.getHeader("AUthorization"))
+            .map(t -> t.replace("Bearer ", "")).orElse(null);
+
+
         Claims claims = tokenService.validateToken(token);
         String username = claims.get("name", String.class);
         @SuppressWarnings("unchecked")
@@ -49,13 +52,25 @@ public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
             .map(role -> new SimpleGrantedAuthority(role)).collect(Collectors.toList());
 
         UsernamePasswordAuthenticationToken authentication =
-          new UsernamePasswordAuthenticationToken(username, null, authorities);
+            new UsernamePasswordAuthenticationToken(username, null, authorities);
         SecurityContextHolder.getContext().setAuthentication(authentication);
-        
-      } catch(RuntimeException e) {
-        chain.doFilter(req, res);
-      }
 
+      } catch (RuntimeException e) {
+        log.debug(e.toString());
+        returnError(e, res);
+      } ;
       chain.doFilter(req, res);
+    }
+    
+    private void returnError(RuntimeException e, HttpServletResponse res) throws IOException {
+      String data = "invalid token";
+      PrintWriter out = res.getWriter();
+      ErrorDTO error = new ErrorDTO(HttpServletResponse.SC_FORBIDDEN, data);
+      data = objectMapper.writeValueAsString(error);
+      res.setContentType("application/json");
+      res.setCharacterEncoding("UTF-8");
+      res.setStatus(HttpServletResponse.SC_FORBIDDEN);
+      out.print(data);
+      out.flush();
     }
 }
